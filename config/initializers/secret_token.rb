d2318020496d99fc522129edbaa1dafcb6bf9088
Oscar Persson <dallas.persson@gmail.com>
2012-03-23 09:22:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vinkallaren::Application.config.secret_token = 'cc9c305406a40af114487156896a90231641da299a28ad8b604577be9cb5fbd1bfbbd98c86789368328e640d8ef72e1b115059711d0afca7746bb165b4a5b3ce'
